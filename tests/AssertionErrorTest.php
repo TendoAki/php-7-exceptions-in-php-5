85e07186781ff5a94c7011aac888c38153efc896
mmorris <mmorris@tombras.com>
2015-07-23 11:24:53 -0400

    Checks if setting ASSERT_EXCEPTION, 1 implicitly sets ASSERT_ACTIVE to 1.

--- tests/AssertionErrorTest.php
+++ tests/AssertionErrorTest.php
@@ -15,15 +15,26 @@ assert_options(ASSERT_ACTIVE, 1);
  */
 class AssertionErrorTest extends PHPUnit_Framework_TestCase {
   /**
-   *
+   * @expectedException AssertionError
    */
   public function testAssertErrorThrow() {
-    try {
+    assert(false);
+  }
+
+  /**
+   * Tests turning assertion exceptions on doesn't turn assertions on in PHP 7.
+   */
+  public function testAssertException() {
+    if (version_compare(PHP_VERSION, '7.0.0-dev') >= 0) {
+      assert_options(ASSERT_ACTIVE, 0);
+      assert_options(ASSERT_EXCEPTION, 0);
+      // This line will be skipped.
       assert(false);
-    } catch (Throwable $e) {
-      $this->assertTrue($e instanceof AssertionError, print_r($e, TRUE));
-    } catch (Exception $e) {
-      $this->assertTrue($e instanceof AssertionError);
+      assert_options(ASSERT_EXCEPTION, 1);
+      // This line SHOULD be skipped.
+      assert(false);
     }
+    // If we reach this line, all is well.
+    $this->assertTrue(TRUE);
   }
 }
